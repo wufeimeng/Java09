78d5d3bcdefe1c0a0ef340ce1dfc8b13cf4d3389
wufeimeng <113487917+wufeimeng@users.noreply.github.com>
2022-10-19 11:28:42 +0800

    Add files via upload

---
+++ class15.java
@@ -0,0 +1,34 @@
+package w87;
+
+class CCircle     
+{
+  
+   private double radius;
+
+   public CCircle(double r)
+   {
+      radius=r;
+   }
+   public static double compare(CCircle c[])  
+   {
+      double average=0.0;
+      for(int i=0;i<c.length;i++)
+    	  average+=c[i].radius;
+      
+      return average/c.length;
+   }
+}
+
+public class test
+{
+   public static void main(String args[])
+   {
+      CCircle cir[];
+      cir=new CCircle[3];
+      cir[0]=new CCircle(1.0);
+      cir[1]=new CCircle(4.0);
+      cir[2]=new CCircle(2.0);
+
+      System.out.println("average = "+CCircle.compare(cir));
+   }
+}
